e80ddd550c903f2e80b8a1d4597ae4a492c08104
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2024-03-08 16:31:21 -0600

    Tarea

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,15 @@
+/*
+export function App(){
+    return ( <h1> Hola mundo </h1> );
+}
+*/
+
+const HelloWorldApp = () => {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
+}
+
+export default HelloWorldApp
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { FirstApp } from "./FirstApp";
+//import HelloWorldApp from "./HelloWorldApp";
 
-function App(){
-    return ( <h1> Hola mundo </h1> );
-}
+
 ReactDOM.createRoot( document.getElementById('root') ).render(
   <React.StrictMode>
-        <App />
+        <FirstApp />
     </React.StrictMode>
 );
