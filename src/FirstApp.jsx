b4a8adaba8012452fbca5886f83e3bed265e799b
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2024-03-08 17:04:03 -0600

    Agregar estilos css

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,23 @@
+
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Jan',
+};
+
+const mensaje = () => {
+    return "Holis";
+}
+
 export const FirstApp = () => {
+
+    
+
     return(
         <>
-            <h1>Jan Luis</h1>
+            <h1> { JSON.stringify( newMessage ) } </h1>
+            <h3> { newMessage.message } </h3>
+            <h4> { newMessage.title } </h4>
+            <h4> { mensaje() } </h4>
             <p>Soy un subtítulo</p>
         </>
         
