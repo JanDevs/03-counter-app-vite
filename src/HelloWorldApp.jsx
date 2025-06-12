63784805739b34dc4f9774063223917edd6c6ad2
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-06-12 13:13:06 -0600

    Primer componente

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+/*
+function App () {
+
+    return <h1>Hola Mundo</h1>;
+}
+export default App;
+*/
+
+
+const HelloWorldApp = () => {
+  return (
+    <div>
+        Hola mundo
+    </div>
+  )
+}
+
+export default HelloWorldApp
